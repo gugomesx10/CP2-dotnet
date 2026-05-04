99e45cf3bdafb6c115f3f4844de74d39bb9ef312
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-04 17:30:33 -0400

    chore: atualizando o program.cs

--- CP2/Program.cs
+++ CP2/Program.cs
@@ -1,41 +1,31 @@
 using CP2.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationContext>(options =>
-{
+builder.Services.AddDbContext<ApplicationContext>(options => {
     options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
 });
 
 builder.Services.AddControllers();
-
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "CP2 API",
-        Version = "v1"
-    });
+builder.Services.AddSwaggerGen(c => { 
+    c.EnableAnnotations();
 });
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CP2 API v1");
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
