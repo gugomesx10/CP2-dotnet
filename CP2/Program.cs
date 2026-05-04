c3e4260f292043421c4a0abbb6e441d7bee9f0a9
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-04 18:00:51 -0400

    fix: corrigindo e deletei algumas versões e inclui versões corretas para subir o projeto

--- CP2/Program.cs
+++ CP2/Program.cs
@@ -1,31 +1,42 @@
 using CP2.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationContext>(options => {
+builder.Services.AddDbContext<ApplicationContext>(options =>
+{
     options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { 
-    c.EnableAnnotations();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CP2 API",
+        Version = "v1"
+    });
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "CP2 API v1");
+    });
 }
 
 app.UseAuthorization();
 
 app.MapControllers();
-
 
 app.Run();
