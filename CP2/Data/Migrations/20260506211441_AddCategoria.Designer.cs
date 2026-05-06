1eefb3e2991decc8aa3b537e1be09913d50c5575
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-06 17:15:56 -0400

    feat: Gerando de forma automática a tabela de Categoria

--- CP2/Data/Migrations/20260506211441_AddCategoria.Designer.cs
+++ CP2/Data/Migrations/20260506211441_AddCategoria.Designer.cs
@@ -2,6 +2,7 @@
 using CP2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -10,9 +11,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace CP2.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20260506211441_AddCategoria")]
+    partial class AddCategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
