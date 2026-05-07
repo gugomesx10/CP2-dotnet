67332059376c9451f4150544ea811c27f3ecf2df
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-05-06 20:10:50 -0400

    chore: atualizei cada entities, eu coloquei os nomes das tabelas de cada classe criada

--- CP2/Entities/Produto.cs
+++ CP2/Entities/Produto.cs
@@ -1,7 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CP2.Entities;
-
+[Table("TB_PRDUTO")]
 public class Produto
 {
     [Key]
